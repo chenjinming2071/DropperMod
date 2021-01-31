6e6fa373c73e0d44d9ffe2dd3a4e92bb8e7c3bf8
chenjinming2071 <chenjinming2071@Outlook.com>
2021-01-31 22:45:03 +0800

    Dropper sound

--- src/main/java/chenjinming2071/liquiddropper/item/ItemDropper.java
+++ src/main/java/chenjinming2071/liquiddropper/item/ItemDropper.java
@@ -1,16 +1,37 @@
 package chenjinming2071.liquiddropper.item;
 
+import chenjinming2071.liquiddropper.liquiddropper.EventBusSubscriber;
 import chenjinming2071.liquiddropper.liquiddropper.LiquidDropper;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.util.*;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.CapabilityInject;
+import net.minecraftforge.fluids.capability.IFluidHandler;
 
 public class ItemDropper extends Item {
     public static final String NAME = "LiquidDropper";
     public static final String REGISTRYNAME = LiquidDropper.MODID + "_" + ItemDropper.NAME;
     public static final String TRANSLATIONKEY = LiquidDropper.MODID + "." + ItemDropper.NAME;
+
     public ItemDropper() {
         this.setMaxStackSize(1);
         this.setCreativeTab(LiquidDropper.LIQUIDDROPPER_TAB);
         this.setRegistryName(ItemDropper.REGISTRYNAME);
         this.setTranslationKey(ItemDropper.TRANSLATIONKEY);
+    }
+
+    @CapabilityInject(IFluidHandler.class)
+    static Capability<IFluidHandler> DROPPER_CAPABILITY = null;
+
+    @Override
+    public EnumActionResult onItemUse(EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+        if (world.isRemote) {
+            System.out.println("test");
+            world.playSound(player, player.posX, player.posY, player.posZ, EventBusSubscriber.DROPPER_SOUND, SoundCategory.NEUTRAL, 0.5F, 0.4F / (itemRand.nextFloat() * 0.4F + 0.8F));
+        }
+        return EnumActionResult.SUCCESS;
     }
 }
--- src/main/java/chenjinming2071/liquiddropper/liquiddropper/EventBusSubscriber.java
+++ src/main/java/chenjinming2071/liquiddropper/liquiddropper/EventBusSubscriber.java
@@ -4,6 +4,8 @@ import chenjinming2071.liquiddropper.item.ItemDropper;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.entity.Entity;
 import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundEvent;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.event.RegistryEvent;
@@ -16,11 +18,16 @@ import net.minecraftforge.registries.IForgeRegistry;
 
 @Mod.EventBusSubscriber(modid = LiquidDropper.MODID)
 public class EventBusSubscriber {
+    public static final SoundEvent DROPPER_SOUND = new SoundEvent(new ResourceLocation(LiquidDropper.MODID, "dropper_sound"));
     public static final ItemDropper ITEM_DROPPER = new ItemDropper();
     @SubscribeEvent
     public static void registerItem(RegistryEvent.Register<Item> event) {
         IForgeRegistry<Item> registry = event.getRegistry();
         registry.register(ITEM_DROPPER);
+    }
+    @SubscribeEvent
+    public static void onSoundEventRegistration(RegistryEvent.Register<SoundEvent> event) {
+        event.getRegistry().register(DROPPER_SOUND.setRegistryName(new ResourceLocation(LiquidDropper.MODID, "dropper_sound")));
     }
     @SubscribeEvent
     public static void onPlayerJoin(EntityJoinWorldEvent event) {
