7bd46f06510ab74c8f35daafe7aebada5141be41
chenjinming2071 <chenjinming2071@Outlook.com>
2021-02-09 21:23:12 +0800

    more mod

--- src/main/java/chenjinming2071/liquiddropper/liquiddropper/LiquidDropper.java
+++ src/main/java/chenjinming2071/liquiddropper/liquiddropper/LiquidDropper.java
@@ -5,8 +5,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(modid = LiquidDropper.MODID, name = LiquidDropper.NAME, version = LiquidDropper.VERSION)
-public class LiquidDropper
-{
+public class LiquidDropper {
     public static final String MODID = "dropper";
     public static final String NAME = "LiquidDropper";
     public static final String VERSION = "1.0";
