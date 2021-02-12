24a58cc7dbc30ab0757d2729fcb089bc2b5b32f1
chenjinming2071 <chenjinming2071@Outlook.com>
2021-02-12 11:11:02 +0800

    complete!!!

--- src/main/java/chenjinming2071/liquiddropper/item/ItemDropper.java
+++ src/main/java/chenjinming2071/liquiddropper/item/ItemDropper.java
@@ -3,6 +3,10 @@ import chenjinming2071.liquiddropper.liquiddropper.EventBusSubscriber;
 import chenjinming2071.liquiddropper.liquiddropper.LiquidDropper;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagInt;
+import net.minecraft.nbt.NBTTagString;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -11,16 +15,30 @@ public class ItemDropper extends Item {
     public static final String NAME = "LiquidDropper";
     public static final String REGISTRYNAME = LiquidDropper.MODID + "_" + ItemDropper.NAME;
     public static final String TRANSLATIONKEY = LiquidDropper.MODID + "." + ItemDropper.NAME;
+
     public ItemDropper() {
         this.setMaxStackSize(1);
         this.setCreativeTab(LiquidDropper.LIQUIDDROPPER_TAB);
         this.setRegistryName(ItemDropper.REGISTRYNAME);
         this.setTranslationKey(ItemDropper.TRANSLATIONKEY);
     }
-    @Override
+
     public EnumActionResult onItemUse(EntityPlayer player, World worldin, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
-        if (worldin.isRemote) {
-            worldin.playSound(player, player.posX, player.posY, player.posZ, EventBusSubscriber.DROPPER_SOUND, SoundCategory.NEUTRAL, 0.5F, 0.4F / (itemRand.nextFloat() * 0.4F + 0.8F));
+        if (!worldin.isRemote) {
+            NBTTagCompound nbt1 = new NBTTagCompound();
+            NBTTagCompound nbt2 = new NBTTagCompound();
+            TileEntity a = worldin.getTileEntity(pos);
+            System.out.println("tileentity:" + a);
+            a.writeToNBT(nbt1);
+            nbt1.setTag("Amount" , new NBTTagInt(0));
+            nbt1.setTag("FluidName" , new NBTTagString(""));
+            nbt1.getCompoundTag("CoolantTank").setTag("FluidName" , new NBTTagString(""));
+            nbt1.getCompoundTag("CoolantTank").setTag("Amount", new NBTTagInt(0));
+            a.readFromNBT(nbt1);
+            System.out.println(nbt1.toString());
+            if (worldin.isRemote) {
+                worldin.playSound(player, player.posX, player.posY, player.posZ, EventBusSubscriber.DROPPER_SOUND, SoundCategory.NEUTRAL, 0.5F, 0.4F / (itemRand.nextFloat() * 0.4F + 0.8F));
+            }
         }
         return EnumActionResult.SUCCESS;
     }
